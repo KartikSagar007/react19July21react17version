a802c8aa9327888c07bd25dee74e644980ad9741
Sagar Bagga <sagarbagga@Sagars-MacBook-Pro.local>
2021-07-19 09:21:57 +0530

    fromvscode921am

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz! - 19th July 2021 - from StackBlitz 9:19am</h1>
       <p>Start editing to see some magic happen :) - from vs code 9:17am</p>
+      <p>from vs code 9:21am</p>
     </div>
   );
 }
