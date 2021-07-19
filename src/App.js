5e3b7a78fd093385faa20eafa9be198b0c7c020a
KartikSagar007 <kartiksagar007@gmail.com>
2021-07-19 09:14:03 +0530

    title edit from stackblitz

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz! - 19th July 2021</h1>
+      <h1>Hello StackBlitz! - 19th July 2021 - from StackBlitz</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
